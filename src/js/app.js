62afcfec5690e15a2f3b12199aeb36f3bd17c23c
Ivan Nikolaenko <nikolaenkostudio@gmail.com>
2023-03-31 09:40:52 +0300

    task had been corrected

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,23 @@
 class ErrorRepository {
   constructor() {
     this.error = new Map();
-  }
-
-  addError(name, description) {
-    this.error.set(name, description);
+    this.error.set(405, "Method Not Allowed");
+    this.error.set(404, "Not Found");
+    this.error.set(502, "Bad Gateway");
   }
 
   translate(code) {
     if (this.error.has(code)) {
       return this.error.get(code);
     }
-    return 'Unknown error';
+    return "Unknown error";
   }
 }
 
 export default ErrorRepository;
 
 const somes = new ErrorRepository();
-somes.addError('Title of error', 'some description about');
-const result = somes.translate('Title of error');
+
+const result = somes.translate(409);
 
 console.log(result);
--- test/app.test.js
+++ test/app.test.js
@@ -1,17 +1,11 @@
-import ErrorRepository from '../src/js/app';
+import ErrorRepository from "../src/js/app";
 
 const somes = new ErrorRepository();
 
-somes.addError('Title of error', 'some description about');
-
-test('test for method addError of class ErrorRepository', () => {
-  expect(somes.error.size).toBe(1);
+test("test for method thanslate(isError) of class ErrorRepository", () => {
+  expect(somes.translate(405)).toBe("Method Not Allowed");
 });
 
-test('test for method thanslate(isError) of class ErrorRepository', () => {
-  expect(somes.translate('Title of error')).toBe('some description about');
-});
-
-test('test for method thanslate(noError) of class ErrorRepository', () => {
-  expect(somes.translate('Some error')).toBe('Unknown error');
+test("test for method thanslate(noError) of class ErrorRepository", () => {
+  expect(somes.translate("Some error")).toBe("Unknown error");
 });
